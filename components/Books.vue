1ecdec4e8a99822aac5d8e24e2a60230e1f5eda7
Bruno Risso <brunorisso58@gmail.com>
2023-03-02 22:13:47 -0300

    Trying to fix error

--- components/Books.vue
+++ components/Books.vue
@@ -1,26 +1,37 @@
 <script setup>
-// import fetchBooks from '../composables/fetchBooks';
-// const books = fetchBooks();
-// console.log(books.value)
+import fetchBooks from '../composables/fetchBooks';
 
-const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '69429c6d52mshbf884f8d222d712p12bb0djsne05d7c0b5898',
-            'X-RapidAPI-Host': 'iq-bible.p.rapidapi.com'
-        } 
-    }
-const req  = await fetch('https://iq-bible.p.rapidapi.com/GetBibleBookAbbreviations', options);
-const data = await req.json();
+// const state = reactive({
+//     data: null
+// })
 
-console.log(data)
+const data = fetchBooks();
+
+const promiseValue = data.then(res => {
+    console.log(res.data.value[0])
+    return res.data.value[0]
+})
+
+// const options = {
+//         method: 'GET',
+//         headers: {
+//             'X-RapidAPI-Key': '69429c6d52mshbf884f8d222d712p12bb0djsne05d7c0b5898',
+//             'X-RapidAPI-Host': 'iq-bible.p.rapidapi.com'
+//         } 
+//     }
+// const req  = await fetch('https://iq-bible.p.rapidapi.com/GetBibleBookAbbreviations', options);
+// const data = await req.json();
+
+// console.log(data)
 </script>
 
 <template>
     <div class="m-12">
         <h1 class="text-4xl font-serif">Select a book:</h1>
         <ul class="text-xl">
-            <li>{{ data }} </li>
+            <li>
+                {{ promiseValue }}
+            </li>
         </ul>
     </div>
 </template>
--- composables/fetchBooks.js
+++ composables/fetchBooks.js
@@ -1,13 +1,12 @@
-// export default async () => {
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Key': '69429c6d52mshbf884f8d222d712p12bb0djsne05d7c0b5898',
-//             'X-RapidAPI-Host': 'iq-bible.p.rapidapi.com'
-//         } 
-//     }
-//     const req  = await fetch('https://iq-bible.p.rapidapi.com/GetBibleBookAbbreviations', options);
-//     const data = await req.json();
+export default async () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '69429c6d52mshbf884f8d222d712p12bb0djsne05d7c0b5898',
+            'X-RapidAPI-Host': 'iq-bible.p.rapidapi.com'
+        } 
+    }
 
-//     return { data }
-// }
+    const { data, error } = await useFetch('https://iq-bible.p.rapidapi.com/GetBibleBookAbbreviations', options);
+    return { data, error };
+}
